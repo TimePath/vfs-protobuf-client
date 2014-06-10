3250998bd77b919998c38a7481cfd3b8132ece71
TimePath <andrew.hardaker1995@gmail.com>
2014-06-10 14:41:42 +1000

    Protect from bogus messages

--- src/main/java/com/timepath/major/ProtoConnection.java
+++ src/main/java/com/timepath/major/ProtoConnection.java
@@ -13,13 +13,17 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import java.lang.reflect.Method;
 import java.net.Socket;
+import java.text.MessageFormat;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * @author TimePath
  */
 public abstract class ProtoConnection {
 
+    private static final Logger LOG = Logger.getLogger(ProtoConnection.class.getName());
     private final OutputStream os;
     private final InputStream  is;
 
@@ -33,31 +37,33 @@ public abstract class ProtoConnection {
     }
 
     private void callback(Meta msg) {
+        if(msg == null) return;
         Map<FieldDescriptor, Object> allFields = msg.getAllFields();
-        for(Object o : allFields.values()) {
-            if(o == null) continue;
-            Method m = null;
+        for(Object field : allFields.values()) {
+            if(field == null) continue;
+            Method callback = null;
             for(Method method : getClass().getDeclaredMethods()) {
-                if(isApplicable(method, o)) {
-                    m = method;
+                if(isApplicable(method, field)) {
+                    callback = method;
                     break;
                 }
             }
-            if(m == null) {
-                throw new RuntimeException("No callback for '" + o.getClass() + "'.");
+            if(callback == null) {
+                LOG.log(Level.WARNING, "No callback for ''{0}''", field);
+                continue;
             }
             try {
-                m.setAccessible(true);
-                m.invoke(this, o);
-            } catch(Exception e) {
-                throw new RuntimeException("Callback failed for '" + o.getClass() + "'.", e);
+                callback.setAccessible(true);
+                callback.invoke(this, field);
+            } catch(Throwable e) {
+                LOG.log(Level.WARNING, MessageFormat.format("Callback failed for ''{0}''", field), e);
             }
         }
     }
 
     private boolean isApplicable(Method method, Object o) {
         if(method.getAnnotation(Callback.class) == null) return false;
-        Class[] c = method.getParameterTypes();
+        Class<?>[] c = method.getParameterTypes();
         if(c.length != 1) return false;
         return c[0].isInstance(o);
     }
